25fb287b15655b252e444e156457b9e012db5274
yangyangisyou <ss880309@gmail.com>
2020-05-23 21:37:45 +0800

    deploy

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,10 @@ export default {
   created() {
     document.title = 'Ubike 地圖';
   },
+  mounted() {
+    const { NODE_ENV } = process.env;
+    document.documentElement.className = NODE_ENV;
+  },
 };
 </script>
 
